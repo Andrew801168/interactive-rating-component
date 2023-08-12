4cc093c264a6150c001576bc0696a6edad0a2ee6
Zaro <111929287+Andrew801168@users.noreply.github.com>
2023-08-12 16:17:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let numbers = Array.from(document.querySelectorAll(".number"));
 let btn = document.querySelector(".rating-box .btn");
 let ratingBox = document.querySelector(".rating-box");
 let thanksBox = document.querySelector(".thanks-box");
-let stars;
+let stars = 0;
 
 function removeActive() {
   numbers.forEach((num) => num.classList.remove("active"));
